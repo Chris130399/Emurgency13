0ffcc583c72280fbf253b3bc1e8ac5775d499671
Chris <chrispius99@gmail.com>
2020-02-14 21:50:43 +0530

    This is the NEW MAPS Module

---
+++ app/src/main/java/com/example/emurgency13/PayPalConfig.java
@@ -0,0 +1,7 @@
+package com.example.emurgency13;
+
+public class PayPalConfig {
+
+    public static final String PAYAPAL_CLIENT_ID = "AVZDVUIXUmdoJ1kc3SaRLZ8QjSAqF5jtgX2NsH8gn-Z4V7qaFEFZWriQN-V5pfivvWocPTzcnQH1zXQg";
+
+}
